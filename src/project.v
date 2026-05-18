1c9484a409c94305cff3f1c767ab1b910e3e27bb
Jahir Burrola <mangopaco29@gmail.com>
2026-05-18 10:34:33 -0600

    Rename module tt_um_Logo_IIT_UACJ2 to tt_um_logo_iit_uacj2

--- src/project.v
+++ src/project.v
@@ -6,7 +6,7 @@
 `default_nettype none
 `define VGA_REGISTERED_OUTPUTS
 
-module tt_um_Logo_IIT_UACJ2 (
+module tt_um_logo_iit_uacj2 (
   input  wire [7:0] ui_in,
   output wire [7:0] uo_out,
   input  wire [7:0] uio_in,
